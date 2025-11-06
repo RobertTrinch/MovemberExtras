72bb5de6f4f17194e50a87e4c5e7564501059b70
Robert <143804444+RobertTrinch@users.noreply.github.com>
2025-11-06 20:22:23 +0000

    Build up the front page

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,12 +1,30 @@
+import '@mantine/core/styles.css';
+
+import { ColorSchemeScript, MantineProvider, createTheme, mantineHtmlProps } from '@mantine/core';
+
+
+export const metadata = {
+  title: 'Movember Extras',
+  description: 'A collection of extra features to help fundraise for Movember',
+};
+
+const theme = createTheme({
+
+});
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" {...mantineHtmlProps}>
+      <head>
+        <ColorSchemeScript />
+      </head>
       <body>
-        {children}
+        <MantineProvider forceColorScheme='dark' theme={theme}>{children}</MantineProvider>
       </body>
     </html>
   );
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,6 +1,58 @@
+import { Button, Card, Container, SimpleGrid, Group, Image, Title, Text } from "@mantine/core";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
+      <Container ta="center" pt="xl" pb="sm">
+        <Title order={1} size="48px" fw="700" c="white">
+          Movember Extras
+        </Title>
+        <Title order={3} fw="500">
+          A collection of extra features to help fundraise for Movember
+        </Title>
+        <Link href="https://movember.com/m/roberttrinch" target="_blank" rel="noopener noreferrer">
+          <Button mt="sm" variant="outline" color="white" size="md" radius="xl">
+            <Image src="/mo_icon_white.png" alt="Movember Logo" height={10} pr="sm" />
+            Donate to Movember
+          </Button>
+        </Link>
+      </Container>
+      <Container ta="center" pt="md" m={0} fluid>
+        <SimpleGrid cols={{ base: 1, sm: 2, lg: 3 }}>
+          <Card shadow="md" padding="md" radius="lg" withBorder>
+            <Group justify="space-between" mt="xs">
+              <Text fw={500}>Movember Overlay</Text>
+            </Group>
+            <Text size="sm" c="dimmed" ta="start">
+              An overlay for your livestreams or website which shows your current Movember progress, updating every 30 seconds.
+            </Text>
+            <Button color="white" variant="outline" fullWidth mt="md" radius="md">
+              Setup Movember Overlay
+            </Button>
+          </Card>
+          <Card shadow="md" padding="md" radius="lg" withBorder>
+            <Group justify="space-between" mt="xs">
+              <Text fw={500}>Coming soon..</Text>
+            </Group>
+          </Card>
+          <Card shadow="md" padding="md" radius="lg" withBorder>
+            <Group justify="space-between" mt="xs">
+              <Text fw={500}>Coming soon..</Text>
+            </Group>
+          </Card>
+        </SimpleGrid>
+      </Container>
+      <Container ta="center">
+        <Text size="sm" c="dimmed" mt="xl" mb="md">
+          <Link href="https://movember.com/m/roberttrinch" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'dimgray', paddingRight: '30px' }}>
+            Donate to Movember
+          </Link>
+          <Link href="https://github.com/RobertTrinch/MovemberExtras" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'dimgray' }}>
+            GitHub Page
+          </Link>
+        </Text>
+      </Container>
     </>
   );
 }
