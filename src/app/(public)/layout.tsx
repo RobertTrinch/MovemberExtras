f48bc87ed330f884b13c606634ee8a21dcd5b667
Robert <143804444+RobertTrinch@users.noreply.github.com>
2025-11-09 02:21:30 +0000

    Metadata + visual tweaks

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -17,6 +17,22 @@ export default function RootLayout({
   return (
     <html lang="en" {...mantineHtmlProps}>
       <head>
+        <title>Movember Extras</title>
+        <meta name="title" content="Movember Extras" />
+        <meta name="description" content="MoExtras: A collection of extra features to help fundraise for Movember, such as Alerts Overlay and Goal Progress!" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://moextras.trinch.net" />
+        <meta property="og:title" content="Movember Extras" />
+        <meta property="og:description" content="MoExtras: A collection of extra features to help fundraise for Movember, such as Alerts Overlay and Goal Progress!" />
+        <meta property="og:image" content="https://moextras.trinch.net/apple-touch-icon.png" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://moextras.trinch.net" />
+        <meta property="twitter:title" content="Movember Extras" />
+        <meta property="twitter:description" content="MoExtras: A collection of extra features to help fundraise for Movember, such as Alerts Overlay and Goal Progress!" />
+        <meta property="twitter:image" content="https://moextras.trinch.net/apple-touch-icon.png" />
+        <meta name="keywords" content="movember,movemberextras,moextras,movember extras,movember overlays" />
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <meta name="language" content="English" />
         <ColorSchemeScript />
       </head>
       <body>
